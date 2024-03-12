34fda77549cf26f5bafeea07e7caca2140f3c1f8
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2024-03-12 22:12:28 +0100

    Xcode new project

---
+++ LIR_ParserPOC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LIR_ParserPOC
+//
+//  Created by Vasile Cotovanu on 12.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
