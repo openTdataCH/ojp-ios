13401b97c5051e865ecdca04ddb700ef10423075
Terence Alberti <terence.alberti@bls.ch>
2024-03-13 21:25:37 +0100

    add library package

---
+++ OjpSDK/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "OjpSDK",
+    platforms: [.iOS(.v13)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "OjpSDK",
+            targets: ["OjpSDK"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "OjpSDK"),
+        .testTarget(
+            name: "OjpSDKTests",
+            dependencies: ["OjpSDK"]),
+    ]
+)
---
+++ OjpSDK/Sources/OjpSDK/OjpSDK.swift
@@ -0,0 +1,47 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+
+
+import Foundation
+
+
+public class OjpSDK {
+    
+    public static let ojpBaseUrl = "https://api.opentransportdata.swiss/ojp2020"
+    
+    let baseUrl: String
+    let customHttpHeaders: [String: String]?
+    
+    public init(baseUrl: String = OjpSDK.ojpBaseUrl, customHttpHeaders: [String: String]?) {
+        self.baseUrl = baseUrl
+        self.customHttpHeaders = customHttpHeaders
+    }
+    
+    public func nearbyStation(from coordinates: (long: Double, lat: Double)) async throws -> Station {
+        
+        // create xml for the request
+        
+        // make the http request
+        
+        // give back the response
+        
+        return Station(name: "Test", latitude: 3.4, longitude: 7.6)
+        
+    }
+    
+    
+    public func stations(from searchTerm: String, count: Int) async throws -> [Station] {
+        
+        // create xml for the request
+        
+        // make the http request
+        
+        // give back the response
+        
+        
+        return [Station(name: "Test", latitude: 3.4, longitude: 7.6)]
+    }
+    
+    
+}
