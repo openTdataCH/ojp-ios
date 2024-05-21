b2ccf0bbdb942d9aa81ec255b78c8bd7f024a058
Reto Lehnherr <reto.lehnherr@ti8m.ch>
2024-05-21 11:44:22 +0200

    Adds more assertions on the minimal triprequest

--- Tests/OJPTests/TripRequestTests.swift
+++ Tests/OJPTests/TripRequestTests.swift
@@ -15,7 +15,7 @@ final class TripRequestTests: XCTestCase {
 
             XCTAssertEqual(trip.id, "ID-E63FDE7C-080D-4FA8-AEC7-EF1C5F31010E")
             switch trip.tripType {
-            case .trip(let trip):
+            case let .trip(trip):
                 let dateFormatter = ISO8601DateFormatter()
                 XCTAssertEqual(dateFormatter.string(from: trip.startTime), "2024-05-14T21:45:00Z")
                 XCTAssertEqual(dateFormatter.string(from: trip.endTime), "2024-05-15T06:40:00Z")
@@ -23,8 +23,37 @@ final class TripRequestTests: XCTestCase {
                 XCTAssertEqual(trip.duration, "PT8H55M")
                 XCTAssertEqual(trip.legs.count, 1)
 
+                guard let leg = trip.legs.first else {
+                    return XCTFail("Missing trip")
+                }
+
+                XCTAssertEqual(leg.id, 1)
+                XCTAssertEqual(leg.duration, "PT15M")
+
+                guard case let .timed(timedLeg) = leg.legType else {
+                    return XCTFail("Expected a timed Leg")
+                }
+
+                let legBoard = timedLeg.legBoard
+                XCTAssertEqual(legBoard.stopPointRef, "8507099")
+                XCTAssertEqual(legBoard.stopPointName.text, "Gurten Kulm")
+                XCTAssertEqual(dateFormatter.string(from: legBoard.serviceDeparture.timetabledTime), "2024-05-14T21:45:00Z")
+
+                XCTAssert(timedLeg.legsIntermediate.count == 0)
+                XCTAssert(timedLeg.legTrack == nil)
+                let legAlight = timedLeg.legAlight
+                XCTAssertEqual(legAlight.stopPointRef, "8507097")
+                XCTAssertEqual(legAlight.stopPointName.text, "Wabern (Gurtenbahn)")
+                XCTAssertEqual(dateFormatter.string(from: legAlight.serviceArrival.timetabledTime), "2024-05-14T22:00:00Z")
+
+                XCTAssertEqual(timedLeg.service.mode.ptMode, .bus)
+
+                let attributeCodes = timedLeg.service.attributes.map(\.code)
+                let attributeTexts = timedLeg.service.attributes.map(\.userText.text)
+                XCTAssertEqual(attributeCodes, ["A__VN", "A__NF"])
+                XCTAssertEqual(attributeTexts, ["VELOS: Keine Beförderung möglich", "Niederflureinstieg"])
             case .tripSummary:
-                XCTFail("Trip Summary is not expected")
+                XCTFail("TripSummary is not expected")
             }
             return
         }
