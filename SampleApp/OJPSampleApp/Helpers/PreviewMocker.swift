5e39f80edee1ac1c6f67ae10a204e9dd5789a2d5
Reto Lehnherr <reto.lehnherr@ti8m.ch>
2024-06-28 09:18:15 +0200

    Add a Mocker for the SwiftUI Previews in SampleApp

---
+++ SampleApp/OJPSampleApp/Helpers/PreviewMocker.swift
@@ -0,0 +1,51 @@
+//
+//  PreviewMocker.swift
+//  OJPSampleApp
+//
+//  Created by Lehnherr Reto on 27.06.2024.
+//
+
+import Foundation
+import OJP
+
+class PreviewMocker {
+
+    static let shared: PreviewMocker = PreviewMocker()
+
+    static func loadXML(xmlFilename: String) throws -> Data {
+        guard let path = Bundle.main.path(forResource: xmlFilename, ofType: "xml") else {
+            throw NSError(domain: "Not Found", code: 1)
+        }
+        return try Data(contentsOf: URL(fileURLWithPath: path))
+    }
+
+    static func mockLoader(xmlFilename: String = "tr-with-transfer-legs") -> LoadingStrategy {
+        .mock { _ in
+            do {
+                let data = try loadXML(xmlFilename: xmlFilename)
+                print("success!")
+                return (data, mockedResponse(statusCode: 200))
+            } catch {
+                return (Data(), mockedResponse(statusCode: 500))
+            }
+        }
+    }
+
+    private static func mockedResponse(statusCode: Int) -> URLResponse {
+        HTTPURLResponse(url: URL(string: "https://localhost")!, statusCode: statusCode, httpVersion: "1.0", headerFields: [:])!
+    }
+
+    func loadTrips() async -> [OJPv2.TripResult] {
+        do {
+            return try await OJP(
+                loadingStrategy: Self.mockLoader())
+            .requestTrips(from: .stopPlaceRef("a", .init("A")),
+                          to: .stopPlaceRef("b", .init("B")),
+                          params: .init()
+            )
+        } catch {
+            print(error.localizedDescription)
+            return []
+        }
+    }
+}
